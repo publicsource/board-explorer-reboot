54e98f009625a92f0a4699f157997d95a1c32ee8
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-02-01 10:18:39 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Board Explorer`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
